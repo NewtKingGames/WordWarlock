cb486bb9c423a2b07e5596c142afc4aeff6665d0
NewtKingGames <newtkinggames@gmail.com>
2024-07-05 12:04:47 -0500

    Fix flying bat bug moving forever

--- Scripts/States/enemy_chase_projectile.gd
+++ Scripts/States/enemy_chase_projectile.gd
@@ -3,15 +3,18 @@ class_name EnemyChaseProjectileState
 
 func Enter():
 	enemy.animated_sprite_2d.play("chase")
+	print("within enemy chase enter")
 
 
 func Update(Delta):
 	var vector_to_player: Vector2 = player.global_position - enemy.global_position
 	if vector_to_player.length() > enemy.chase_distance:
 		Transitioned.emit(self, "idle")
+		return
 	# Should probably type cast here
 	if vector_to_player.length() <= enemy.attack_distance_to_enter:
 		Transitioned.emit(self, "attack")
+		return
 	enemy.velocity = vector_to_player.normalized() * enemy.walk_speed
 	enemy.animated_sprite_2d.play("chase")
 
