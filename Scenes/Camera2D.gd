3ff9d34ea08889ef15dcc779574d969bad486f39
NewtKingGames <newtkinggames@gmail.com>
2024-07-09 20:30:21 -0500

    Adding camera zoom when player starts casting

--- Scenes/Camera2D.gd
+++ Scenes/Camera2D.gd
@@ -2,28 +2,37 @@ extends Camera2D
 
 @export var random_strength: float = 50
 @export var shake_fade: float = 10
+@export var typing_zoom_rate: float = 5
 
 var rng = RandomNumberGenerator.new()
 
 var shake_strength: float = 0
 var player: Player
+var camera_target_zoom: Vector2 = zoom
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	player = get_tree().get_first_node_in_group("player")
 	Globals.connect("player_damage", apply_shake)
 	player.connect("casting_state_entered", on_casting_state_entered)
+	player.connect("casting_state_exited", on_casting_state_exited)
 	
 func _process(delta):
 	if shake_strength > 0:
 		shake_strength = lerpf(shake_strength, 0, shake_fade*delta)
+	if zoom != camera_target_zoom:
+		zoom.x = lerpf(zoom.x, camera_target_zoom.x, typing_zoom_rate*delta)
+		zoom.y = lerpf(zoom.y, camera_target_zoom.y, typing_zoom_rate*delta)
 	offset = random_offset()
 	
 func apply_shake():
 	shake_strength = random_strength
 
 func on_casting_state_entered():
-	print("player started typing")
+	camera_target_zoom = Vector2(2, 2)
+	
+func on_casting_state_exited():
+	camera_target_zoom = Vector2(1.4, 1.4)
 	
 func random_offset() -> Vector2:
 	return Vector2(rng.randf_range(-shake_strength, shake_strength), rng.randf_range(-shake_strength, shake_strength))
