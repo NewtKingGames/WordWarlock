1fa074f50ad448b704ba4ccce4dc948d5a31d230
NewtKingGames <newtkinggames@gmail.com>
2024-08-04 22:34:59 -0500

    Update boomerang

--- Scripts/Spells/boomerang.gd
+++ Scripts/Spells/boomerang.gd
@@ -1,23 +1,43 @@
 extends ProjectileSpell
 class_name Boomerang
 
-var max_distance: float = 200
+@onready var cpu_particles_2d:CPUParticles2D = $CPUParticles2D
+
+var max_distance: float = 300
 var distance_traveled: float = 0
 var retrievable_distance: float = 10
 var starting_position: Vector2
 var traveling_back: bool = false
+var starting_speed: float
+var ending_speed: float
+var current_speed: float
 var player: Player
-
+const RETRO_SWOOOSH_02 = preload("res://Sounds/GameSFX/Swoosh/Retro Swooosh 02.wav")
 # TODOs
-# [] Have the boomerang collide with walls
+# [X] Have the boomerang return to the player after hitting a wall
+# [] Have the boomerang actualy collide with walls so it can't travel through them?
 # [] Solve the bug that the above^ will introduce by adding a timer until the boomerang is returned
-# [] See if you can make the speed start fast and then slow down on each path
+# [X] See if you can make the speed start fast and then slow down on each path <- This could still use a little bit of polish
 # [] Add particle effects
-# [] Add lighting
+# ^ you need to set the velocity of those particles to be the direction of the boomerang
+# [X] Add lighting
+# [X] Add sounds
 
 func _ready():
 	starting_position = global_position
 	player = get_tree().get_first_node_in_group("player")
+	starting_speed = spell_speed * 2
+	ending_speed = spell_speed
+	current_speed = starting_speed
+	super._ready()
+
+func _physics_process(delta):
+	# Have the boomerang slow down towards it's destination and then speed up on the way back
+	if not traveling_back:
+		current_speed = lerpf(current_speed, ending_speed, delta*8)
+	else:
+		current_speed = lerpf(current_speed, starting_speed, delta*8)
+	print(current_speed)
 
 # Do we need a state machine?
 # I think we actually don't want this to collide with enemies
@@ -26,20 +46,33 @@ func _process(delta):
 	if traveling_back:
 		direction = (player.global_position - global_position).normalized()
 	# Move the boomerang and calculate distance traveled
-	position += direction*spell_speed*delta
+	position += direction*current_speed*delta
 	distance_traveled = (starting_position - global_position).length()
 	if distance_traveled >= max_distance:
 		traveling_back = true
+		print("the speed was")
+		print(current_speed)
+	cpu_particles_2d.direction = direction
+	
 	# Traveling back and hit the player
 	if traveling_back and (player.global_position - global_position).length() < retrievable_distance:
+		print("the speed was")
+		print(current_speed)
 		queue_free()
 
 # OVERRIDE
 static func get_spell_color():
 	return Color.WEB_PURPLE
 
-
+func boomerang_hit_body(body):
+	spell_hit_body(body)
+	# Flash a light or other animations?
+	$PointLight2D.energy = 1.5
+	await get_tree().create_timer(.1).timeout
+	$PointLight2D.energy = 0
+	
 func _on_body_entered(body):
-	# If we hit a wall we return the boomerang back to the player
+	boomerang_hit_body(body)
+	 #If we hit a wall we return the boomerang back to the player
 	if is_instance_of(body, TileMap):
 		traveling_back = true
