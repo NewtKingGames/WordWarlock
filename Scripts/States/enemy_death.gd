8f29a410fcd856e145b4f18ecac6207d9af07e48
NewtKingGames <newtkinggames@gmail.com>
2024-08-05 23:09:57 -0500

    Add new sprite death animation

--- Scripts/States/enemy_death.gd
+++ Scripts/States/enemy_death.gd
@@ -4,4 +4,11 @@ class_name EnemyDeath
 
 func Enter():
 	enemy.velocity = Vector2.ZERO
+	if is_instance_of(enemy, Slime):
+		# Randomize death animation for slime
+		if randi_range(0, 1) == 1:
+			enemy.animated_sprite_2d.play("death")
+		else:
+			enemy.animated_sprite_2d.play("death_two")
+		return
 	enemy.animated_sprite_2d.play("death")
