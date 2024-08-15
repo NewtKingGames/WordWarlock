fda49f1e3790d7f6ca5e638e2edc88effb2a6193
NewtKingGames <newtkinggames@gmail.com>
2024-08-14 23:01:54 -0500

    Remove print

--- Scenes/objects/torch.gd
+++ Scenes/objects/torch.gd
@@ -6,16 +6,10 @@ var max_torch_energy: float = randf_range(0.25, 0.35)
 var min_torch_energy: float = randf_range(0.05, 0.15)
 var initial_torch_energy: float = randf_range(min_torch_energy, max_torch_energy)
 
-
 # The goal of this is replicate the animation player lighting effect, but in code so that you can randomize it
 func _ready():
-	print(max_torch_energy)
-	print(min_torch_energy)
 	point_light_2d.energy = initial_torch_energy
-	pass
-	# TODO which tween?
 	animate_lights()
-	#pass
 
 func animate_lights():
 	var light_flicker_tween: Tween = create_tween().set_loops()
