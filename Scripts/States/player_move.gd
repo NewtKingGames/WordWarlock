61db7ef746047269ddf838a65af69be6bf80c12f
NewtKingGames <newtkinggames@gmail.com>
2024-09-04 21:42:10 -0500

    Anytime player clicks enter make the player cast

--- Scripts/States/player_move.gd
+++ Scripts/States/player_move.gd
@@ -9,13 +9,14 @@ func Enter():
 	character_animated_sprite_2d.play("walk")
 	
 func Update(_delta: float):
+	if Input.is_action_just_pressed("enter"):
+		player.velocity = Vector2.ZERO
+		Transitioned.emit(self, "cast")
+		return
 	var direction = Input.get_vector("left", "right", "up", "down")
 	player.velocity = direction * Globals.player_walk_speed
 	if not direction:
-		if Input.is_action_just_pressed("enter"):
-			Transitioned.emit(self, "cast")
-		else:
-			Transitioned.emit(self, "idle")
+		Transitioned.emit(self, "idle")
 	if player.velocity.x > 0:
 		player.character_animated_sprite.flip_h = false
 	elif player.velocity.x < 0:
