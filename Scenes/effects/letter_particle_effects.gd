86dda048524e131f01d066b069957b5ccec755f9
NewtKingGames <newtkinggames@gmail.com>
2024-11-24 20:31:00 -0600

    Tweak spell letter particle effects

--- Scenes/effects/letter_particle_effects.gd
+++ Scenes/effects/letter_particle_effects.gd
@@ -11,7 +11,12 @@ func _process(delta: float) -> void:
 	speed_scale = 1 / Engine.time_scale
 
 func _on_spell_equipped(spell: SpellResource) -> void:
-	# Leave the first black partially transparent point the same, set the last color to the primary color?
-	print(spell.primary_color)
+	var clear_primary: Color = Color(spell.primary_color)
+	clear_primary.a = 0
+	var clear_secondary: Color = Color(spell.secondary_color)
+	clear_secondary.a = 0
+	color_ramp.set_color(0, clear_primary)
 	color_ramp.set_color(1, spell.primary_color)
+	color_ramp.set_color(2, spell.secondary_color)
+	color_ramp.set_color(3, clear_secondary)
 # The original purple color I set for this node was Color(0.514, 0.404, 1) 
