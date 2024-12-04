54da25aef792533510a3e6bf6f546230d36bfbce
NewtKingGames <newtkinggames@gmail.com>
2024-12-03 19:52:51 -0600

    Change visibility layer of some text and import possible shader

---
+++ Scenes/ui/color_rect.gd
@@ -0,0 +1,17 @@
+extends ColorRect
+
+
+func _ready() -> void:
+	#var tween: Tween = create_tween()
+	#set_shader_value(0)
+	#tween.tween_method(set_shader_value, 0.0, 1.0, 1)
+	Events.player_entered_casting_state.connect(_on_player_entered_slow_mo)
+
+func set_shader_value(value: float) -> void:
+	material.set_shader_parameter("radius", value)
+
+
+func _on_player_entered_slow_mo() -> void:
+	set_shader_value(0.8)
+	#var tween: Tween = create_tween()
+	#tween.tween_method(set_shader_value, 0.0, 1.0, 0.2)
