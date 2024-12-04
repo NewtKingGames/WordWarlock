d50e47b2c6ad1cdb78048a2abd3f997b06073472
NewtKingGames <newtkinggames@gmail.com>
2024-12-04 17:57:01 -0600

    Changes to the spike parent scene to support setting spikes in the editor and dynamic creation

--- Scenes/objects/spike_parent.gd
+++ Scenes/objects/spike_parent.gd
@@ -1,9 +1,20 @@
+# @tool allows this code to be ran in the editor
+@tool
 class_name SpikeParent
 extends Node2D
-@onready var spike_nodes: Node2D = $SpikeNodes
+const SPIKES_SCENE: PackedScene = preload("res://Scenes/objects/spikes.tscn")
 @onready var activate_spike_noise: AudioStreamPlayer2D = $ActivateSpikeNoise
 @onready var deactivate_spike_noise: AudioStreamPlayer2D = $DeactivateSpikeNoise
-
+@export var num_spikes_x: int = 0:
+	set(num):
+		num_spikes_x = num
+		_delete_all_spikes()
+		create_spikes()
+@export var num_spikes_y: int = 0:
+	set(num):
+		num_spikes_y = num
+		_delete_all_spikes()
+		create_spikes()
 
 #@export var auto_start_spikes: bool = true
 @export var is_spikes_on_timer: bool = true
@@ -12,18 +23,38 @@ extends Node2D
 @export var deactivate_spike_delay: float = 1.5
 
 func _ready() -> void:
-	get_tree().create_timer(activate_spike_delay).timeout.connect(activate_spikes)
+	# Only run this ready function when scene is loaded outside of the editor
+	if not Engine.is_editor_hint():
+		create_spikes()
+		get_tree().create_timer(activate_spike_delay).timeout.connect(activate_spikes)
+
+func create_spikes() -> void:
+	# Create the spikes:
+	var spike_position: Vector2 = position
+	for x in range(num_spikes_x):
+		for y in range(num_spikes_y):
+			var spikes: Spikes = SPIKES_SCENE.instantiate()
+			spikes.position = Vector2(63*x, 63*y)
+			add_child(spikes)
+			if Engine.is_editor_hint():
+				spikes.owner = self
 
 func activate_spikes() -> void:
 	activate_spike_noise.play()
-	for spike_child in spike_nodes.get_children():
+	for spike_child in get_children():
 		if spike_child is Spikes:
 			spike_child.activate_spikes()
 	get_tree().create_timer(deactivate_spike_delay).timeout.connect(deactivate_spikes)
 
 func deactivate_spikes() -> void:
 	deactivate_spike_noise.play()
-	for spike_child in spike_nodes.get_children():
+	for spike_child in get_children():
 		if spike_child is Spikes:
 			spike_child.deactivate_spikes()
 	get_tree().create_timer(deactivate_spike_delay).timeout.connect(activate_spikes)
+
+
+func _delete_all_spikes() -> void:
+	for child in get_children():
+		if child is Spikes:
+			child.queue_free()
