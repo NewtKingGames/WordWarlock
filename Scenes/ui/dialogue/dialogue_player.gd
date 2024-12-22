ddfffdcea0b4092b676e3e211c5796cb149c5823
NewtKingGames <newtkinggames@gmail.com>
2024-12-22 16:29:51 -0600

    Allow prompts to be skippable

--- Scenes/ui/dialogue/dialogue_player.gd
+++ Scenes/ui/dialogue/dialogue_player.gd
@@ -12,10 +12,12 @@ var casting_text_flow_over: Array[CastingTextParent] = []
 @export_multiline var prompt_array: Array[String] = []
 @export var letter_delay_max: float = 0.08
 @export var letter_delay_min: float = 0.03
+@export var is_skippable_prompt: bool = true
 var starting_prompt_index: int = 0
 var current_prompt_index: int = 0
 var is_playing_prompt: bool = false
 var is_player_near: bool = false
+var is_prompt_skipped: bool = false
 
 func _ready() -> void:
 	keyboard_letter.hide()
@@ -27,14 +29,15 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if is_player_near:
 		# Display the interact button
-		if Input.is_action_just_pressed("interact") and not is_playing_prompt and not Globals.is_player_casting:
-			keyboard_letter.key_pressed()
-			if current_prompt_index == prompt_array.size():
-				reset_prompt()
+		if Input.is_action_just_pressed("interact") and not Globals.is_player_casting:
+			if is_playing_prompt and is_skippable_prompt:
+				short_circuit_prompt(current_prompt_index)
 			else:
-				play_prompt(current_prompt_index)
-				current_prompt_index+=1
-
+				keyboard_letter.key_pressed()
+				if current_prompt_index == prompt_array.size():
+					reset_prompt()
+				else:
+					play_prompt(current_prompt_index)
 
 func _on_player_entered(body: Node2D) -> void:
 	if body is Player:
@@ -53,6 +56,7 @@ func reset_prompt() -> void:
 	clear_prompt()
 
 func play_prompt(index: int) -> void:
+	is_prompt_skipped = false
 	#casting_text_parent.clear_letters()
 	clear_prompt()
 	current_casting_text_parent_index = 0
@@ -60,14 +64,35 @@ func play_prompt(index: int) -> void:
 	var prompt: String = prompt_array[index] as String
 	if not prompt:
 		return
-	for character in prompt:
-		if character == "\n":
-			current_casting_text_parent_index += 1
-		casting_texts.get_child(current_casting_text_parent_index).add_letter(character)
+	for letter in prompt:
+		if is_prompt_skipped:
+			return
+		_add_letter_to_prompt(letter)
 		typing_noises.play_typing_noise()
 		await get_tree().create_timer(randf_range(letter_delay_min, letter_delay_max)).timeout
+	_prompt_finished()
+
+# Function to support the player pressing 'E' during the initial prompt
+func short_circuit_prompt(index: int) -> void:
+	is_prompt_skipped = true
+	clear_prompt()
+	current_casting_text_parent_index = 0
+	var prompt: String = prompt_array[index] as String
+	if not prompt:
+		return
+	for letter in prompt:
+		_add_letter_to_prompt(letter)
+	typing_noises.play_typing_noise()
+	_prompt_finished()
+
+func _prompt_finished() -> void:
 	is_playing_prompt = false
-	
+	current_prompt_index+=1
+
+func _add_letter_to_prompt(letter: String) -> void:
+	if letter == "\n":
+			current_casting_text_parent_index += 1
+	casting_texts.get_child(current_casting_text_parent_index).add_letter(letter)
 
 func clear_prompt() -> void:
 	for casting_text_parent in casting_texts.get_children():
