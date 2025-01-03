4aeeeec1334eacd144b221bc366c07f33e98e7d2
NewtKingGames <newtkinggames@gmail.com>
2025-01-03 16:07:24 -0600

    Continue to flesh out level flows

--- Scenes/levels/hub_level.gd
+++ Scenes/levels/hub_level.gd
@@ -2,18 +2,25 @@ extends Node2D
 
 
 # TODO - this should have an init function to allow for it to be passed in to constructor
-var level_transition_context: LevelTransitionContext
+var level_transition_context: LevelTransitionContext = LevelTransitionContext.new()
 
 @onready var book_case_tile_slider_left: TileSlider = $BookCaseTileSliderLeft
 @onready var book_case_tile_slider_right: TileSlider = $BookCaseTileSliderRight
 
+@onready var intro_dialogue_player: DialoguePlayer = %IntroDialoguePlayer
+
+
+# TODO - delete, used for testing the animation
+var test_var: bool = false
 
 var player: Player
 @onready var player_spawn_return_from_level: Marker2D = %PlayerSpawn_ReturnFromLevel
 
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group("player") as Player
-	if level_transition_context and level_transition_context.transition_reason == LevelTransitionContext.TRANSITION_REASON.LEVEL_COMPLETED:
+	if test_var or (level_transition_context and level_transition_context.transition_reason == LevelTransitionContext.TRANSITION_REASON.LEVEL_COMPLETED):
+		# Disable the intro dialogue player
+		intro_dialogue_player.queue_free()
 		player.global_position = player_spawn_return_from_level.global_position
 		# Lock player controls
 		player.toggle_player_input(false)
@@ -32,4 +39,10 @@ func _ready() -> void:
 		book_case_tile_slider_left.slide_back()
 		await book_case_tile_slider_right.slide_back()
 		# Release player control again
+		player.toggle_player_input(true)
+	elif level_transition_context.transition_reason == LevelTransitionContext.TRANSITION_REASON.INITIAL_GAME_LOAD:
+		# Disable player control and play the fun animation
+		player.toggle_player_input(false)
+		player.character_animated_sprite.play("reanimate")
+		await get_tree().create_timer(0.625).timeout
 		player.toggle_player_input(true)
--- Scripts/ContextObjects/level_transition_context.gd
+++ Scripts/ContextObjects/level_transition_context.gd
@@ -9,10 +9,11 @@ static var level_names_to_scenes: Dictionary = {
 # TODO - consider having the resource accept the packed scene as well
 enum LEVEL_NAME {HUB_LEVEL, TEST_LEVEL, FIRST_LEVEL}
 
-enum TRANSITION_REASON {LEVEL_COMPLETED, LEVEL_EXITED, PLAYER_DIED, INITIAL_GAME_LOAD}
+enum TRANSITION_REASON {INITIAL_GAME_LOAD, LEVEL_COMPLETED, LEVEL_EXITED, PLAYER_DIED}
 
 @export var level_name: LEVEL_NAME
 @export var transition_reason: TRANSITION_REASON
+
 
 
 func get_packed_level_scene() -> PackedScene:
